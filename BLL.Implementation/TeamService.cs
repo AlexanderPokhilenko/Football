80641443044c17a4c1ef1b07affb6a82e25c92ad
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2020-06-10 16:10:47 +0300

    Реализация BLL.

---
+++ BLL.Implementation/TeamService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using BLL.Abstract;
+using DAL.Abstract.Repositories;
+using DTO;
+
+namespace BLL.Implementation
+{
+    public class TeamService : ITeamService
+    {
+        private readonly ITeamRepository teamRep;
+        private readonly ICountryRepository countryRep;
+
+        public TeamService(ITeamRepository teamRep, ICountryRepository countryRep)
+        {
+            this.teamRep = teamRep;
+            this.countryRep = countryRep;
+        }
+
+        public void AddTeam(TeamDto t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TeamDto> GetTeams()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BLL.Implementation/TournamentService.cs
@@ -0,0 +1,37 @@
+﻿using BLL.Abstract;
+using DAL.Abstract.Repositories;
+using DTO;
+using System;
+using System.Collections.Generic;
+
+namespace BLL.Implementation
+{
+    public class TournamentService : ITournamentService
+    {
+        private readonly ITournamentRepository tournamentRep;
+        private readonly IStageRepository stageRep;
+        private readonly IStageTypeRepository stageTypeRep;
+
+        public TournamentService(ITournamentRepository tournamentRep, IStageRepository stageRep, IStageTypeRepository stageTypeRep)
+        {
+            this.tournamentRep = tournamentRep;
+            this.stageRep = stageRep;
+            this.stageTypeRep = stageTypeRep;
+        }
+
+        public void AddParticipation(int tournamentId, ParticipationDto p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TournamentDto GetTournament(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TourPreviewDto> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
