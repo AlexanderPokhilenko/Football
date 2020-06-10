f5846c46fb125e843f8305319df2df35a92daaaf
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2020-06-10 16:20:22 +0300

    Добавил пару свойств.

--- DTO/ParticipationDto.cs
+++ DTO/ParticipationDto.cs
@@ -2,6 +2,8 @@
 {
     public class ParticipationDto
     {
+        public string StageTypeName { get; set; }
+        public string TournamentName { get; set; }
         public string FirstTeamName { get; set; }
         public string SecondTeamName { get; set; }
         public byte FirstTeamPoints { get; set; }
